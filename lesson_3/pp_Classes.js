ffb1622099c375eb498dba3fdb49e229ed7148a0
Deepak Karivelil <deepakkarivelil@gmail.com>
2022-03-24 17:33:12 -0300

    working through lesson 3

---
+++ lesson_3/pp_Classes.js
@@ -0,0 +1,18 @@
+// 1
+/*
+In JavaScript, classes are first-class values. This means that classes can be used anywhere a value is used.
+They can be passed around to functions, returned from functions, and assigned to variables.
+*/
+
+// 2
+class Telivision {
+  static manufacturer() {
+
+  }
+
+  model() {
+
+  }
+}
+
+// The `static` modifier makes it so that the method `manufacturer()` can only be called on the class `Telivision`. It cannot be called on instances of `Telivision`. `model` on the other hand can only be called by instances of the `Telivision` object.
