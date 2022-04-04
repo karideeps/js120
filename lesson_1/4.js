5491c2176dcb68796f3249e4874b2a372fedacf3
Deepak Karivelil <deepakkarivelil@gmail.com>
2022-04-04 13:18:48 -0300

    worked on lesson 1

---
+++ lesson_1/4.js
@@ -0,0 +1,21 @@
+/*
+
+1.
+OOP is a programming paradigm in which we think about the problem in terms of objects. The idea is to model a program based on how objects in the real world interact.
+
+2.
+Advantages:
+  Allows programmers to think at a higher level of abstraction
+  Allows the programmer to write code with less "dependencies" in a program. This way, the programmer can work on one part of the code without worrying about breaking other parts of the code.
+
+Disadvantages:
+  It can take up more memory.
+  It can lead to less efficient code. OOP may require more memory, disk space, and computing power.
+
+3.
+Encapsulation is the principle of bundling or combining data and the operations that work on that data into a single entity, e.g., an object. 
+
+4.
+In other OOP languages, encapsulation also refers to restricting access to certain states and behaviors; an object only exposes the data (state and behaviors) that other parts of the application need to work. In other words, objects expose a public interface for interacting with other objects and keep their implementation details hidden. Thus, other objects can't change the data of an object without going through the proper interface.
+
+*/
